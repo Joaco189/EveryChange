e62473db252353ec3fe844358178585c41fd1e79
Joaco189 <mangoldjoaquin@gmail.com>
2022-10-17 16:04:03 -0300

    CheckStorage function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 const formExchange = document.getElementById("form-exchange");
 
+const checkSessionStorage = () => {
+    const storage = JSON.parse(sessionStorage.getItem("change"))
+    const resultDiv = document.getElementById("result");
+    const div = document.createElement("div")
+    div.className = "result"
+    div.innerHTML = `
+                    <div>
+                        <p class="result-number">${storage.result}</p>   
+                    </div>
+                    <div>
+                        <p class="result-date">Ultima actualización: ${storage.date}</p>   
+                    </div> 
+                    `
+    resultDiv.append(div);
+}
+
 const checkInputMoney = () =>{
     const inputMoney = document.getElementById("input-money");
     const i = inputMoney.selectedIndex;
@@ -14,36 +30,60 @@ const checkOutputMoney = () =>{
     return outputMoneySelected;
 }
 
-
-const fetching = async (input, inputMoney, outputMoney) => {
-        const response = await fetch(`https://criptoya.com/api/satoshitango/${inputMoney}/${outputMoney}`)
-        const data = await response.json();
-        console.log(data)
-        const result = input * data.ask;
-        return result
+const fetching = async (inputMoney, outputMoney) => {
+    const response = await fetch(`https://criptoya.com/api/satoshitango/${inputMoney}/${outputMoney}`)
+    const data = await response.json();
+    const result = data.ask
+    const resultObject = {
+        result: result,
+        timestamp: data.time,
+        outputMoney: outputMoney
+    }
+    return resultObject;
 } 
 
-const showResult = async (result) => {
+const showResult = async (input, {result, timestamp, outputMoney}) => {
+    let date = new Date(timestamp*1000).toLocaleString();
+    const resultWFormat = input * result
+    const resultFormat = (resultWFormat.toLocaleString("es",{
+        style: "currency",
+	    currency: `${outputMoney.toUpperCase()}`
+    }))
     const resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = ""
     const div = document.createElement("div")
+    div.className = "result"
     div.innerHTML = `
-                    <p>${result}</p>    
+                    <div>
+                        <p class="result-number">${resultFormat}</p>   
+                    </div>
+                    <div>
+                        <p class="result-date">Ultima actualización: ${date}</p>   
+                    </div> 
                     `
     resultDiv.append(div);
+    const storage = {
+        result: resultFormat,
+        date: date
+    }
+    sessionStorage.setItem("change", JSON.stringify(storage));
 }
 
+if(sessionStorage.getItem("change")){
+    checkSessionStorage()
+}
 
 formExchange.addEventListener("submit", async (e) => {
     e.preventDefault();
     const input = document.getElementById("input").value;
-    if (isNaN(input) || input === 0){
+    if (isNaN(input) || input === 0 || input === ""){
         alert("Tenes que introducir un numero")
     } else{
         const inputMoney = checkInputMoney();
         const outputMoney = checkOutputMoney();
-        console.log(outputMoney)
-        const result = await fetching(input, inputMoney, outputMoney)
-        showResult(result)
+        const result = await fetching(inputMoney, outputMoney)
+        showResult(input, result)
+
     }
     
 })
