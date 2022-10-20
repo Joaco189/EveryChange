039436090b81e8d7a03c98701d0a1a005884c70c
Joaco189 <mangoldjoaquin@gmail.com>
2022-10-20 19:33:31 -0300

    document formatting

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const formExchange = document.getElementById("form-exchange");
 
 const checkSessionStorage = () => {
-    const storage = JSON.parse(sessionStorage.getItem("change"))
+    const storage = JSON.parse(sessionStorage.getItem("change"));
     const resultDiv = document.getElementById("result");
-    const div = document.createElement("div")
-    div.className = "result"
+    const div = document.createElement("div");
+    div.className = "result";
     div.innerHTML = `
                     <div>
                         <p class="result-number">${storage.result}</p>   
@@ -12,47 +12,49 @@ const checkSessionStorage = () => {
                     <div>
                         <p class="result-date">Ultima actualización: ${storage.date}</p>   
                     </div> 
-                    `
+                    `;
     resultDiv.append(div);
-}
+};
 
-const checkInputMoney = () =>{
+const checkInputMoney = () => {
     const inputMoney = document.getElementById("input-money");
     const i = inputMoney.selectedIndex;
     const inputMoneySelected = inputMoney.options[i].value;
     return inputMoneySelected;
-}
+};
 
-const checkOutputMoney = () =>{
+const checkOutputMoney = () => {
     const outputMoney = document.getElementById("output-money");
     const i = outputMoney.selectedIndex;
     const outputMoneySelected = outputMoney.options[i].value;
     return outputMoneySelected;
-}
+};
 
 const fetchingCurrency = async (inputMoney, outputMoney) => {
-    const response = await fetch(`https://criptoya.com/api/satoshitango/${inputMoney}/${outputMoney}`)
+    const response = await fetch(
+        `https://criptoya.com/api/satoshitango/${inputMoney}/${outputMoney}`
+    );
     const data = await response.json();
-    const result = data.ask
+    const result = data.ask;
     const resultObject = {
         result: result,
         timestamp: data.time,
-        outputMoney: outputMoney
-    }
+        outputMoney: outputMoney,
+    };
     return resultObject;
-} 
+};
 
-const showResult = async (input, {result, timestamp, outputMoney}) => {
-    let date = new Date(timestamp*1000).toLocaleString();
-    const resultWFormat = input * result
-    const resultFormat = (resultWFormat.toLocaleString("es",{
+const showResult = async (input, { result, timestamp, outputMoney }) => {
+    let date = new Date(timestamp * 1000).toLocaleString();
+    const resultWFormat = input * result;
+    const resultFormat = resultWFormat.toLocaleString("es", {
         style: "currency",
-	    currency: `${outputMoney.toUpperCase()}`
-    }))
+        currency: `${outputMoney.toUpperCase()}`,
+    });
     const resultDiv = document.getElementById("result");
-    resultDiv.innerHTML = ""
-    const div = document.createElement("div")
-    div.className = "result"
+    resultDiv.innerHTML = "";
+    const div = document.createElement("div");
+    div.className = "result";
     div.innerHTML = `
                     <div>
                         <p class="result-number">${resultFormat}</p>   
@@ -60,98 +62,97 @@ const showResult = async (input, {result, timestamp, outputMoney}) => {
                     <div>
                         <p class="result-date">Ultima actualización: ${date}</p>   
                     </div> 
-                    `
+                    `;
     resultDiv.append(div);
     const storage = {
         result: resultFormat,
-        date: date
-    }
+        date: date,
+    };
     sessionStorage.setItem("change", JSON.stringify(storage));
-}
+};
 
-if(sessionStorage.getItem("change")){
-    checkSessionStorage()
+if (sessionStorage.getItem("change")) {
+    checkSessionStorage();
 }
 
 formExchange.addEventListener("submit", async (e) => {
     e.preventDefault();
     const input = document.getElementById("input").value;
-    if (isNaN(input) || input === 0 || input === ""){
+    if (isNaN(input) || input === 0 || input === "") {
         Toastify({
             text: "Tenes que introducir un numero!",
             duration: 2000,
-            gravity: "bottom", 
-            position: "right", 
+            gravity: "bottom",
+            position: "right",
             stopOnFocus: true,
             className: "font",
             style: {
-                background: "linear-gradient(90deg, rgba(135,65,100,1) 0%, rgba(255,91,91,1) 100%)",
+                background:
+                    "linear-gradient(90deg, rgba(135,65,100,1) 0%, rgba(255,91,91,1) 100%)",
             },
         }).showToast();
-    } else{
+    } else {
         const inputMoney = checkInputMoney();
         const outputMoney = checkOutputMoney();
-        const result = await fetchingCurrency(inputMoney, outputMoney)
-        showResult(input, result)
-
+        const result = await fetchingCurrency(inputMoney, outputMoney);
+        showResult(input, result);
     }
-    
-})
+});
 
 // parte usd
 
 const fetchingUsd = async () => {
-    const response = await fetch(`https://criptoya.com/api/dolar`)
-    const data = await response.json()
-    return data
-}
+    const response = await fetch(`https://criptoya.com/api/dolar`);
+    const data = await response.json();
+    return data;
+};
 
 const showUsdSection = async () => {
     const usdContainer = document.getElementById("container-usd");
-    const objectUsd = await fetchingUsd() 
-    const usdDate = new Date(objectUsd.time*1000).toLocaleString();
-    console.log(usdDate)
+    const objectUsd = await fetchingUsd();
+    const usdDate = new Date(objectUsd.time * 1000).toLocaleString();
+    console.log(usdDate);
     const div = document.createElement("div");
-    div.style = "margin-top: 0.5rem;"
-    div.nth
+    div.style = "margin-top: 0.5rem;";
+    div.nth;
     div.innerHTML = `
                         <div class="container-usd-type">
                             <p>Dolar Oficial</p>
-                            <span>${objectUsd.oficial.toLocaleString("es",{
+                            <span>${objectUsd.oficial.toLocaleString("es", {
                                 style: "currency",
-                                currency: `ARS`
+                                currency: `ARS`,
                             })}</span>
                         </div>
                         <div class="container-usd-type">
                             <p>Dolar Blue</p>
-                            <span>${objectUsd.blue.toLocaleString("es",{
+                            <span>${objectUsd.blue.toLocaleString("es", {
                                 style: "currency",
-                                currency: `ARS`
+                                currency: `ARS`,
                             })}</span>
                         </div>
                         <div class="container-usd-type">
                             <p>Dolar CCB</p>
-                            <span>${objectUsd.ccb.toLocaleString("es",{
+                            <span>${objectUsd.ccb.toLocaleString("es", {
                                 style: "currency",
-                                currency: `ARS`
+                                currency: `ARS`,
                             })}</span>
                         </div>
                         <div class="container-usd-type last">
                             <p>Dolar CCL</p>
-                            <span>${objectUsd.ccl.toLocaleString("es",{
+                            <span>${objectUsd.ccl.toLocaleString("es", {
                                 style: "currency",
-                                currency: `ARS`
+                                currency: `ARS`,
                             })}</span>
                         </div>
                         <div class="usdDate">
                             <p>Ultima actualización: ${usdDate}</p>
                         </div>
-                    `
+                    `;
     usdContainer.append(div);
     setInterval(() => {
-        usdContainer.removeChild(div)
-        showUsdSection()
-    },  30000); 
-}
+        usdContainer.removeChild(div);
+        showUsdSection();
+    }, 30000);
+};
 
-showUsdSection()
+showUsdSection();
